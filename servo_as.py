bbf1e86cd683ef51917d16c3f9b1607dd211884c
Philip Leach <phil@pa-leach.com>
2022-10-25 14:57:01 +0100

    Working (sort of)!

---
+++ servo_as.py
@@ -0,0 +1,79 @@
+from math import degrees
+import uasyncio as asyncio
+from machine import Pin, PWM
+
+class Servo_SG90:
+    
+    DUTY_RESOLUTION = 65535
+    FREQUENCY = 50
+
+    ONE_SECOND = 1000
+    ONE_MS = 1
+    TWO_MS = 2
+    MIN_DUTY = (DUTY_RESOLUTION // (ONE_SECOND//FREQUENCY)) * ONE_MS
+    MAX_DUTY = (DUTY_RESOLUTION // (ONE_SECOND//FREQUENCY)) * TWO_MS
+    DUTY_PER_DEGREE = (MAX_DUTY - MIN_DUTY) // 180 
+
+    CENTER = 0
+    PLUS_NINETY = 90
+    MINUS_NINETY = -90
+
+    def __init__(self,name : str, pin_number:int, initial_position: int = 0,rotational_velocity:int = 30):
+
+        print(f"MIN_DUTY {Servo_SG90.MIN_DUTY}, MAX_DUTY {Servo_SG90.MAX_DUTY}, DUTY_PER_DEGREE {Servo_SG90.DUTY_PER_DEGREE}")
+        self.name = name
+        self.pin_number = pin_number
+        self.initial_position = self._limit_range(initial_position)
+        self.rotational_velocity = rotational_velocity
+        
+        self.servo_pwm = PWM(Pin(self.pin_number))
+        self.servo_pwm.freq(Servo_SG90.FREQUENCY)
+
+        self.position = self.initial_position
+        self.servo_pwm.duty_u16(self._degrees_to_duty(self.position))
+        print(f'position {self.position}, Duty {self._degrees_to_duty(self.position)} {self.servo_pwm.duty_u16()}')
+        self.lock = asyncio.Lock()
+
+    async def move_to_poistion(self, degrees:int):
+        print(f"Move to {degrees} degrees")
+        await self.lock.acquire()
+        degrees = self._limit_range(degrees)
+
+        for step in self._steps(degrees):
+            self.servo_pwm.duty_u16(step)
+            await asyncio.sleep_ms(10)  
+
+        self.position = degrees  
+        self.lock.release()
+        
+    def _limit_range(self, degrees: int) -> int:
+        if degrees > Servo_SG90.PLUS_NINETY:
+            return Servo_SG90.PLUS_NINETY
+        elif degrees < Servo_SG90.MINUS_NINETY:
+            return Servo_SG90.MINUS_NINETY
+        else:
+            return degrees
+
+    def _degrees_to_duty(self, degrees:int) -> int:
+        return Servo_SG90.MIN_DUTY + (((degrees + Servo_SG90.PLUS_NINETY) * (Servo_SG90.MAX_DUTY - Servo_SG90.MIN_DUTY)) // 180)
+
+    def _steps(self, degrees) -> list:
+
+        if degrees < self.position:
+            return list(reversed(self._generate_step_values(degrees, self.position)))          
+        else:
+            return self._generate_step_values(self.position, degrees)
+            
+
+    def _generate_step_values(self, low:int, high:int) :
+        delta = high - low
+        duty_delta = self._degrees_to_duty(high) - self._degrees_to_duty(low)
+        no_of_steps = delta * 100 // self.rotational_velocity 
+
+        print(f'{no_of_steps} steps to move {delta} degrees')
+        steps = []
+        for x in range(no_of_steps):
+            y = Servo_SG90.MIN_DUTY + ((duty_delta * x) // no_of_steps)
+            steps.append(y) 
+        return steps
+        
--- servo_test.py
+++ servo_test.py
@@ -1,17 +1,9 @@
-# servo_as
-
-A small asynchronous MicroPython servo library for conrolling SG90 servos with a Raspberry Pi Pico.
-
-**Note:** This is a very incomplete WIP project. It may run, and your welcome to browse but please don't depend on it as everything about it is subject to change!
-
-Example Usage :
-```python
 import uasyncio as asyncio
 from servo_as import Servo_SG90 as Servo
 
 async def main():
     servo1 =  Servo('Servo 1',0,Servo.CENTER) 
-    
+    print('Starting...')
     while True:
         asyncio.create_task(servo1.move_to_poistion(Servo.PLUS_NINETY))
         await asyncio.sleep(20)
@@ -20,4 +12,3 @@ async def main():
         
 
 asyncio.run(main())
-```
